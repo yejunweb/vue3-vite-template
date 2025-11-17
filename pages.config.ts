5bebe6c2e1b245cf6578ade35b79c08b79333d10
yejun <2512636001@qq.com>
2025-11-17 11:11:19 +0800

    fix(pages): 更新组件命名前缀从 fg- 到 wg-
    将页面配置中的组件命名前缀由 fg- 更改为 wg-，以保持与实际组件文件名的一致性。
    同时保留了 uni-app 的 easycom 自动扫描配置注释链接，方便后续查阅相关文档。

--- pages.config.ts
+++ pages.config.ts
@@ -9,10 +9,11 @@ export default defineUniPages({
         navigationBarTextStyle: 'black',
         backgroundColor: '#FFFFFF',
     },
+    // https://uniapp.dcloud.net.cn/collocation/pages.html#easycom
     easycom: {
         autoscan: true,
         custom: {
-            '^fg-(.*)': '@/components/fg-$1/fg-$1.vue',
+            '^wg-(.*)': '@/components/wg-$1/wg-$1.vue',
             '^(?!z-paging-refresh|z-paging-load-more)z-paging(.*)':
         'z-paging/components/z-paging$1/z-paging$1.vue',
             '^wd-(.*)': 'wot-design-uni/components/wd-$1/wd-$1.vue',
