48d6db3d2e169cc6a8b2fb97385039716b1ef847
yejun <2512636001@qq.com>
2025-11-17 09:07:19 +0800

    feat(index): 移除首页冗余内容和日志打印
    移除了 `src/pages/index/index.vue` 页面中不再需要的描述文本、作者信息、官网链接等内容，同时删除了无意义的日志输出语句，使页面更加简洁

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -12,11 +12,6 @@ definePage({
     },
 })
 
-const description = ref(
-    'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite5 + UnoCss + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
-)
-console.log('index/index 首页打印了')
-
 onLoad(() => {
     console.log('测试 uni API 自动引入: onLoad')
 })
@@ -29,25 +24,6 @@ onLoad(() => {
         </view>
         <view class="mt-4 text-center text-4xl text-[#d14328]">
             unibest
-        </view>
-        <view class="mb-8 mt-2 text-center text-2xl">
-            最好用的 uniapp 开发模板
-        </view>
-
-        <view class="m-auto mb-2 max-w-100 text-justify indent text-4">
-            {{ description }}
-        </view>
-        <view class="mt-4 text-center">
-            作者：
-            <text class="text-green-500">
-                菲鸽
-            </text>
-        </view>
-        <view class="i-my-icons-copyright mt-4 text-center">
-            官网地址：
-            <text class="text-green-500">
-                https://unibest.tech
-            </text>
         </view>
     </view>
 </template>
